16b26365501601335165b71dffeab922f865d1e0
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2023-12-28 02:32:15 -0500

    Create huffheap.h

---
+++ huffheap.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <bitset> //the bitset library will likely be needed for phases 2 and 3
+#include <string>
+#include "huffman.h"
+
+class HuffHeap {
+//things/variables used in phase 2
+private:
+	Node* arrayTree[257];
+	int subscript = 0;
+	int parent = 0;
+	int leftChild = 0;
+	int rightChild = 0;
+	int tallyNum = 0;
+	void upHeap(int k);
+	void downHeap(int k);
+public:
+	HuffHeap(unordered_map<char, int> tally);
+	void printTrees();
+	void buildHeap();
+};
+
